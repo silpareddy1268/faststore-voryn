59d57a38f00333c0b5ac94222b8401dc5a89e289
silpa.b <silpa.b@echidnainc.com>
2026-07-16 10:57:35 +0530

    add to cart

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,13 +2,11 @@ import CustomIconsAlert from "./sections/CustomIconsAlert/CustomIconsAlert";
 import AlertWithImage from "./sections/AlertWithImage/AlertWithImage";
 import CustomProductDetails from "./sections/CustomProductDetails/CustomProductDetails";
 import CustomNewsletter from "./sections/CustomNewsletter/CustomNewsletter";
-import ContactForm from "./ContactForm/ContactForm";
 
 const sections = {
   CustomIconsAlert,
   AlertWithImage,
   ProductDetails: CustomProductDetails,
-  ContactForm,
   CustomNewsletter,
 };
 
